e6de2e746e3a5a071b78d2a8fb93b81ca102b63f
OneUnknownDev1 <zadenferguson567@gmail.com>
2025-05-13 13:12:57 -0700

    Controls, Cameras

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,16 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.InputSystem;
+using TMPro;
 
-public class Controller : MonoBehaviour
+public class FPSController : MonoBehaviour
 {
-    [Header("Gravity Settings")]
-    [SerializeField] private float gravity = 9.8f;
-
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
 
     [Header("Camera Settings")]
-    [SerializeField] private bool invertYAxis = false; 
-    
- 
+    [SerializeField] private bool invertYAxis = false;
+
+    public bool isWalking;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravity = 9.81f;
+
+    [Header("Look Sensitivity")]
+    [SerializeField] private float mouseSensitivity = 2.0f;
+    [SerializeField] private float upDownRange = 80.0f;
+
+    [Header("Audio")]
+
+
+
+    private CharacterController characterController;
+    private Camera mainCamera;
+    private PlayerInputHandler inputHandler;
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+        inputHandler = PlayerInputHandler.Instance;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+
+
+    void HandleMovement()
+    {
+        float speed = walkSpeed * (inputHandler.SprintValue > 0 ? sprintMultiplier : 1f);
+
+        Vector3 inputDirection = new Vector3(inputHandler.MoveInput.x, 0f, inputHandler.MoveInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        worldDirection.Normalize();
+
+        currentMovement.x = worldDirection.x * speed;
+        currentMovement.z = worldDirection.z * speed;
+        isWalking = true;
+
+        HandleJumping();
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void HandleJumping()
+    {
+        if (characterController.isGrounded)
+        {
+            currentMovement.y = -0.5f;
+
+            if (inputHandler.JumpTriggered)
+            {
+                currentMovement.y = jumpForce;
+
+
+            }
+        }
+        else
+        {
+            currentMovement.y -= gravity * Time.deltaTime;
+        }
+    }
+
+    void HandleRotation()
+    {
+        float mouseYInput = invertYAxis ? -inputHandler.LookInput.y : inputHandler.LookInput.y;
+
+        float mouseXRotation = inputHandler.LookInput.x * mouseSensitivity;
+        transform.Rotate(0, mouseXRotation, 0);
+
+        verticalRotation -= mouseYInput * mouseSensitivity;
+        verticalRotation -= inputHandler.LookInput.y * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        Quaternion quaternion = Quaternion.Euler(verticalRotation, 0, 0);
+        mainCamera.transform.localRotation = quaternion;
+    }
 }
+
